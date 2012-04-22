244691e0469ad7624a3af48ea0cb26b4c4b2dfc6
James A. Rosen <james.a.rosen@gmail.com>
2012-04-22 09:46:28 -0700

    DeductingCalculator takes deduction function
    Although the IncomeTaxCalculator can use a constant deduction, the
    AMT calculator's deductions are a function of the taxable value. Now
    the AMT calculator can be rewritten as a pure combination of the
    Deducting- and Tiered calculators with no additional logic.

--- spec/DeductingTaxCalculatorSpec.js
+++ spec/DeductingTaxCalculatorSpec.js
@@ -1,23 +1,30 @@
 describe("DeductingTaxCalculator#taxOn", function() {
-  var calc, baseCalc;
+  var baseCalc,
+
+      deductingCalc = function(fn) {
+        return new DeductingTaxCalculator(fn, baseCalc);
+      };
 
   beforeEach(function() {
     baseCalc = { taxOn: function() { return -1; } };
     spyOn(baseCalc, 'taxOn').andReturn(100);
-    calc = new DeductingTaxCalculator(10000, baseCalc);
   });
 
   it("returns what the base calculator returns", function() {
+    calc = deductingCalc(function(value) { return 1; });
     expect(calc.taxOn(328253)).toEqual(100);
   });
 
   it("doesn't deduct more than the value taxed", function() {
+    calc = deductingCalc(function(value) { return 10000; });
     calc.taxOn(9999);
     expect(baseCalc.taxOn).toHaveBeenCalledWith(0);
   });
 
-  it("doesn't deduct more than the deduction", function() {
-    calc.taxOn(20000);
-    expect(baseCalc.taxOn).toHaveBeenCalledWith(10000);
+  it('supports dynamic deductions based on the value', function() {
+    calc = deductingCalc(function(value) { return value / 10; });
+    calc.taxOn(150);
+    expect(baseCalc.taxOn).toHaveBeenCalledWith(135);
   });
+
 });
