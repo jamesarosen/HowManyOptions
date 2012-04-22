a2b9e10c030d857b31141b1d596f69b293c93c44
James A. Rosen <james.a.rosen@gmail.com>
2012-04-22 09:57:40 -0700

    all calculators are now pure functions, not objects with a #taxOn method

--- spec/DeductingTaxCalculatorSpec.js
+++ spec/DeductingTaxCalculatorSpec.js
@@ -1,30 +1,29 @@
-describe("DeductingTaxCalculator#taxOn", function() {
+describe("DeductingTaxCalculator", function() {
   var baseCalc,
 
       deductingCalc = function(fn) {
-        return new DeductingTaxCalculator(fn, baseCalc);
+        return DeductingTaxCalculator(fn, baseCalc);
       };
 
   beforeEach(function() {
-    baseCalc = { taxOn: function() { return -1; } };
-    spyOn(baseCalc, 'taxOn').andReturn(100);
+    baseCalc = jasmine.createSpy().andReturn(100);
   });
 
   it("returns what the base calculator returns", function() {
     calc = deductingCalc(function(value) { return 1; });
-    expect(calc.taxOn(328253)).toEqual(100);
+    expect(calc(328253)).toEqual(100);
   });
 
   it("doesn't deduct more than the value taxed", function() {
     calc = deductingCalc(function(value) { return 10000; });
-    calc.taxOn(9999);
-    expect(baseCalc.taxOn).toHaveBeenCalledWith(0);
+    calc(9999);
+    expect(baseCalc).toHaveBeenCalledWith(0);
   });
 
   it('supports dynamic deductions based on the value', function() {
     calc = deductingCalc(function(value) { return value / 10; });
-    calc.taxOn(150);
-    expect(baseCalc.taxOn).toHaveBeenCalledWith(135);
+    calc(150);
+    expect(baseCalc).toHaveBeenCalledWith(135);
   });
 
 });
--- spec/TieredTaxCalculatorSpec.js
+++ spec/TieredTaxCalculatorSpec.js
@@ -1,19 +1,19 @@
-describe("TieredTaxCalculator#taxOn", function() {
+describe("TieredTaxCalculator", function() {
   var calc;
 
   beforeEach(function() {
-    calc = new TieredTaxCalculator( [ [ 0.1, 10000 ], [ 0.2, 20000 ], [ 0.3 ] ] );
+    calc = TieredTaxCalculator( [ [ 0.1, 10000 ], [ 0.2, 20000 ], [ 0.3 ] ] );
   });
 
   it('returns 0 for 0 income', function() {
-    expect(calc.taxOn(0)).toEqual(0);
+    expect(calc(0)).toEqual(0);
   });
 
   it("doesn't apply higher tax brackets until they're reached", function() {
-    expect(calc.taxOn(5000)).toEqual( 5000 * 0.1 );
+    expect(calc(5000)).toEqual( 5000 * 0.1 );
   });
 
   it("applies tax brackets once they're reached", function() {
-    expect(calc.taxOn(21000)).toEqual( 10000 * 0.1 + 10000 * 0.2 + 1000 * 0.3 );
+    expect(calc(21000)).toEqual( 10000 * 0.1 + 10000 * 0.2 + 1000 * 0.3 );
   });
 });
